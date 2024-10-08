67c17edd2edd9768bffcc1bce23df6ccd86aac30
Emmanuel Rosa <emmanuelrosa@protonmail.com>
2024-10-08 09:46:27 -0400

    reorganize flake packages

--- flake.nix
+++ flake.nix
@@ -9,13 +9,17 @@
 
   outputs = { self, nixpkgs, nix-appimage }: {
 
-    packages.x86_64-linux.bisq-desktop = nixpkgs.legacyPackages.x86_64-linux.callPackage ./pkgs/bisq-desktop {
+    packages.x86_64-linux = let
+      system = "x86_64-linux";
+    in {
+      bisq-desktop = nixpkgs.legacyPackages.x86_64-linux.callPackage ./pkgs/bisq-desktop {
         makeBinPath = nixpkgs.legacyPackages.x86_64-linux.lib.makeBinPath;
         openjdk11 = nixpkgs.legacyPackages.x86_64-linux.openjdk11.override { enableJavaFX = true; };
-    };
+      };
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.bisq-desktop;
-    packages.x86_64-linux.bisq-desktop-appimage = nix-appimage.bundlers.x86_64-linux.default self.packages.x86_64-linux.bisq-desktop;
+      bisq-desktop-appimage = nix-appimage.bundlers.x86_64-linux.default self.packages.x86_64-linux.bisq-desktop;
+      default = self.packages.x86_64-linux.bisq-desktop;
+    };
 
   };
 }
